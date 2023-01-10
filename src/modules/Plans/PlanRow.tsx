5714ec37389bac8498064fec43c3a7d25a31ef18
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-01-10 20:13:28 +0100

    Support multiple namespaces
    Key points:
    1. pass namespace via URL (match.params.ns) and then propagate down to
       the query level
    2. drop constant KubeResource instances (used as query templates)
    3. support 'All namespaces' KubeResources (namespace is undefined then)
    4. add ENV.DEFAULT_NAMESPACE constant to be used when no namespace can
       be inferred
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/modules/Plans/PlanRow.tsx
+++ src/modules/Plans/PlanRow.tsx
@@ -26,6 +26,7 @@ interface CellProps {
   value: string;
   entity: FlatPlan;
   primaryAction?: string;
+  currentNamespace: string;
 }
 
 const TextCell = ({ value }: CellProps) => <>{value ?? ''}</>;
@@ -72,7 +73,7 @@ const StatusCell = ({ entity: { status, type, vmCount, vmDone, name, object } }:
   }
 };
 
-const Actions = ({ primaryAction, entity }: CellProps) => {
+const Actions = ({ primaryAction, entity, currentNamespace }: CellProps) => {
   const isBeingStarted = entity.status === 'Starting';
   return (
     <Flex
@@ -98,13 +99,20 @@ const Actions = ({ primaryAction, entity }: CellProps) => {
               plan={entity.object}
               buttonType={primaryAction}
               isBeingStarted={isBeingStarted}
+              currentNamespace={currentNamespace}
             />
           )}
         </FlexItem>
       )}
       {(primaryAction || !isBeingStarted) && (
         <FlexItem align={{ default: 'alignRight' }}>
-          <PlanActions {...{ entity, ignoreList: primaryAction ? [primaryAction] : [] }} />
+          <PlanActions
+            {...{
+              entity,
+              ignoreList: primaryAction ? [primaryAction] : [],
+              namespace: currentNamespace,
+            }}
+          />
         </FlexItem>
       )}
     </Flex>
@@ -139,7 +147,7 @@ const cellCreator: Record<string, (props: CellProps) => JSX.Element> = {
   ),
 };
 
-const PlanRow = ({ columns, entity }: RowProps<FlatPlan>) => {
+const PlanRow = ({ columns, entity, currentNamespace }: RowProps<FlatPlan>) => {
   const { t } = useTranslation();
   const primaryAction = getButtonState(entity.status);
   return (
@@ -152,7 +160,12 @@ const PlanRow = ({ columns, entity }: RowProps<FlatPlan>) => {
             dataLabel={toLabel(t)}
             modifier={id === 'description' ? 'breakWord' : undefined}
           >
-            <Cell value={String(entity[id] ?? '')} entity={entity} primaryAction={primaryAction} />
+            <Cell
+              value={String(entity[id] ?? '')}
+              entity={entity}
+              primaryAction={primaryAction}
+              currentNamespace={currentNamespace}
+            />
           </Td>
         );
       })}
