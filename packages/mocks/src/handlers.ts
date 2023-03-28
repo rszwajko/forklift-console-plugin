29dbd41383e85525475e792b7c41373d24004f6b
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-03-28 20:10:56 +0200

    Add mock package
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

---
+++ packages/mocks/src/handlers.ts
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+
+const ENV = { PLUGIN_NAME: 'forklift-console-plugin' };
+
+const openshiftProvider1 = {
+  uid: 'mock-uid-ocpv-1',
+  namespace: 'openshift-migration',
+  name: 'ocpv-1',
+  selfLink: '/foo/openshiftprovider/1',
+  type: 'openshift',
+  vmCount: 26,
+  networkCount: 8,
+};
+
+export const getInventoryApiUrl = (relativePath?: string): string =>
+  `/api/proxy/plugin/${ENV.PLUGIN_NAME}/forklift-inventory/${relativePath || ''}`;
+
+export const handlers = [
+  rest.get(getInventoryApiUrl('providers'), (req, res, ctx) =>
+    res(ctx.status(200), ctx.json([openshiftProvider1])),
+  ),
+];
