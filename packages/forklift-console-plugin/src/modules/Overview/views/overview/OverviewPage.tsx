87236a5819aac399eb0f6ba779232311ee1f9521
Radoslaw Szwajkowski <rszwajko@redhat.com>
2023-08-22 22:11:21 +0200

    Minimal OverviewPage to reproduce double scrollbars
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- packages/forklift-console-plugin/src/modules/Overview/views/overview/OverviewPage.tsx
+++ packages/forklift-console-plugin/src/modules/Overview/views/overview/OverviewPage.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
-import { InventoryNotReachable } from 'src/modules/Providers/views/list/components';
-import { useForkliftTranslation } from 'src/utils/i18n';
 
-import { HorizontalNav, K8sModel } from '@openshift-console/dynamic-plugin-sdk';
-import { PageSection } from '@patternfly/react-core';
-
-import { useK8sWatchForkliftController, useProvidersInventoryIsLive } from '../../hooks';
-import { getOperatorPhase } from '../../utils/helpers/getOperatorPhase';
-
-import OperatorStatus from './components/OperatorStatus';
-import { HeaderTitle } from './components';
-import { ForkliftControllerDetailsTab, ForkliftControllerYAMLTab } from './tabs';
+import { HorizontalNav, K8sModel, ResourceLink } from '@openshift-console/dynamic-plugin-sdk';
 
 import './OverviewPage.style.css';
 
 export const OverviewPage: React.FC<OverviewPageProps> = () => {
-  const { t } = useForkliftTranslation();
-
-  const pages = [
-    {
-      href: '',
-      name: t('Overview'),
-      component: ForkliftControllerDetailsTabWrapper,
-    },
-    {
-      href: 'yaml',
-      name: t('YAML'),
-      component: ForkliftControllerYAMLTabWrapper,
-    },
-  ];
-
   return (
     <>
-      <HeaderTitleWrapper />
-      <HorizontalNav pages={pages.filter((p) => p)} />
+      <HorizontalNav
+        pages={[
+          {
+            href: '',
+            name: 'Overview',
+            component: () => (
+              <>
+                <div style={{ width: '300px', height: '300px', background: 'green' }} />
+                <ResourceLink
+                  groupVersionKind={{ version: 'v1', kind: 'Namespace' }}
+                  name={'konveyor-forklift'}
+                  namespace={''}
+                />
+              </>
+            ),
+          },
+        ]}
+      />
     </>
   );
 };
@@ -45,43 +36,6 @@ type OverviewPageProps = {
   match: { path: string; url: string; isExact: boolean; params: unknown };
   name: string;
   namespace?: string;
-};
-
-const HeaderTitleWrapper: React.FC = () => {
-  const [forkliftController] = useK8sWatchForkliftController();
-  const { loadError: inventoryLivelinessError } = useProvidersInventoryIsLive({});
-
-  const { t } = useForkliftTranslation();
-
-  const phaseObj = getOperatorPhase(forkliftController);
-
-  return (
-    <>
-      <HeaderTitle
-        title={t('Migration Toolkit for Virtualization')}
-        status={<OperatorStatus status={phaseObj.phase} />}
-      />
-      {inventoryLivelinessError && (
-        <PageSection>{[<InventoryNotReachable key={'inventoryNotReachable'} />]}</PageSection>
-      )}
-    </>
-  );
-};
-
-const ForkliftControllerDetailsTabWrapper: React.FC = () => {
-  const [forkliftController, loaded, loadError] = useK8sWatchForkliftController();
-
-  return (
-    <ForkliftControllerDetailsTab obj={forkliftController} loaded={loaded} loadError={loadError} />
-  );
-};
-
-const ForkliftControllerYAMLTabWrapper: React.FC = () => {
-  const [forkliftController, loaded, loadError] = useK8sWatchForkliftController();
-
-  return (
-    <ForkliftControllerYAMLTab obj={forkliftController} loaded={loaded} loadError={loadError} />
-  );
 };
 
 export default OverviewPage;
