db69d3cf0247c6f70718158df08318e434374af3
Radoslaw Szwajkowski <rszwajko@redhat.com>
2024-01-30 22:47:12 +0100

    move fetch/save to hooks
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- packages/forklift-console-plugin/src/modules/Providers/views/migrate/ProvidersCreateVmMigrationPage.tsx
+++ packages/forklift-console-plugin/src/modules/Providers/views/migrate/ProvidersCreateVmMigrationPage.tsx
@@ -1,239 +1,24 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC } from 'react';
 import { useHistory } from 'react-router';
-import { produce } from 'immer';
 import SectionHeading from 'src/components/headers/SectionHeading';
 import { useForkliftTranslation } from 'src/utils/i18n';
-import { useImmerReducer } from 'use-immer';
 
-import {
-  NetworkMapModel,
-  NetworkMapModelGroupVersionKind,
-  PlanModel,
-  PlanModelGroupVersionKind,
-  PlanModelRef,
-  ProviderModelGroupVersionKind,
-  ProviderModelRef,
-  StorageMapModel,
-  V1beta1NetworkMap,
-  V1beta1Plan,
-  V1beta1Provider,
-  V1beta1StorageMap,
-} from '@kubev2v/types';
-import {
-  k8sCreate,
-  K8sModel,
-  k8sPatch,
-  useK8sWatchResource,
-} from '@openshift-console/dynamic-plugin-sdk';
 import { Alert, Button, Flex, FlexItem, PageSection } from '@patternfly/react-core';
 
 import { useToggle } from '../../hooks';
-import { useNamespaces } from '../../hooks/useNamespaces';
-import { useOpenShiftNetworks, useSourceNetworks } from '../../hooks/useNetworks';
-import { useNicProfiles } from '../../hooks/useNicProfiles';
-import { getResourceUrl } from '../../utils';
 
-import {
-  setAvailableProviders,
-  setAvailableSourceNetworks,
-  setAvailableTargetNamespaces,
-  setAvailableTargetNetworks,
-  setError,
-  setExistingNetMaps,
-  setExistingPlans,
-  setNicProfiles,
-  startCreate,
-} from './actions';
+import { startCreate } from './actions';
 import { PlansCreateForm } from './PlansCreateForm';
-import { useCreateVmMigrationData } from './ProvidersCreateVmMigrationContext';
-import { reducer } from './reducer';
-import { createInitialState, getObjectRef } from './stateHelpers';
+import { useFetchEffects } from './useFetchEffects';
+import { useSaveEffect } from './useSaveEffect';
 
 const ProvidersCreateVmMigrationPage: FC<{
   namespace: string;
 }> = ({ namespace }) => {
   const { t } = useForkliftTranslation();
   const history = useHistory();
-
-  const { data: { selectedVms = [], provider: sourceProvider = undefined } = {} } =
-    useCreateVmMigrationData();
-  // error state - the page was entered directly without choosing the VMs
-  const emptyContext = !selectedVms?.length || !sourceProvider;
-  // error recovery - redirect to provider list
-  useEffect(() => {
-    if (emptyContext) {
-      history.push(
-        getResourceUrl({
-          reference: ProviderModelRef,
-          namespace: namespace,
-        }),
-      );
-    }
-  }, [emptyContext]);
-
-  const [state, dispatch] = useImmerReducer(
-    reducer,
-    { namespace, sourceProvider, selectedVms },
-    createInitialState,
-  );
-  const {
-    workArea: { targetProvider },
-  } = state;
-
-  const [providers, providersLoaded, providerError] = useK8sWatchResource<V1beta1Provider[]>({
-    groupVersionKind: ProviderModelGroupVersionKind,
-    namespaced: true,
-    isList: true,
-    namespace,
-  });
-  useEffect(
-    () => dispatch(setAvailableProviders(providers, providersLoaded, providerError)),
-    [providers],
-  );
-
-  const [plans, plansLoaded, plansError] = useK8sWatchResource<V1beta1Plan[]>({
-    groupVersionKind: PlanModelGroupVersionKind,
-    namespaced: true,
-    isList: true,
-    namespace,
-  });
-  useEffect(
-    () => dispatch(setExistingPlans(plans, plansLoaded, plansError)),
-    [plans, plansLoaded, plansError],
-  );
-
-  const [netMaps, netMapsLoaded, netMapsError] = useK8sWatchResource<V1beta1NetworkMap[]>({
-    groupVersionKind: NetworkMapModelGroupVersionKind,
-    namespaced: true,
-    isList: true,
-    namespace,
-  });
-  useEffect(
-    () => dispatch(setExistingNetMaps(netMaps, netMapsLoaded, netMapsError)),
-    [netMaps, netMapsLoaded, netMapsError],
-  );
-
-  const [namespaces, nsLoading, nsError] = useNamespaces(targetProvider);
-  useEffect(
-    () => dispatch(setAvailableTargetNamespaces(namespaces, nsLoading, nsError)),
-    [namespaces, nsLoading, nsError],
-  );
-
-  const [targetNetworks, targetNetworksLoading, targetNetworksError] =
-    useOpenShiftNetworks(targetProvider);
-  useEffect(
-    () =>
-      dispatch(
-        setAvailableTargetNetworks(targetNetworks, targetNetworksLoading, targetNetworksError),
-      ),
-    [targetNetworks, targetNetworksLoading, targetNetworksError],
-  );
-
-  const [sourceNetworks, sourceNetworksLoading, sourceNetworksError] =
-    useSourceNetworks(sourceProvider);
-  useEffect(
-    () =>
-      dispatch(
-        setAvailableSourceNetworks(sourceNetworks, sourceNetworksLoading, sourceNetworksError),
-      ),
-    [sourceNetworks, sourceNetworksLoading, sourceNetworksError],
-  );
-
-  const [nicProfiles, nicProfilesLoading, nicProfilesError] = useNicProfiles(sourceProvider);
-  useEffect(
-    () => dispatch(setNicProfiles(nicProfiles, nicProfilesLoading, nicProfilesError)),
-    [nicProfiles, nicProfilesLoading, nicProfilesError],
-  );
-
-  const mounted = useRef(true);
-  useEffect(
-    () => () => {
-      mounted.current = false;
-    },
-    [],
-  );
-
-  useEffect(() => {
-    const {
-      flow,
-      underConstruction: { plan, netMap, storageMap },
-    } = state;
-    if (!flow.editingDone || !mounted.current) {
-      return;
-    }
-
-    const createStorage = async (storageMap: V1beta1StorageMap) => {
-      return await k8sCreate({
-        model: StorageMapModel,
-        data: storageMap,
-      });
-    };
-    const createNetwork = async (netMap: V1beta1NetworkMap) => {
-      return await k8sCreate({
-        model: NetworkMapModel,
-        data: netMap,
-      });
-    };
-
-    const createPlan = async (
-      plan: V1beta1Plan,
-      netMap: V1beta1NetworkMap,
-      storageMap: V1beta1StorageMap,
-    ) => {
-      const created = await k8sCreate({
-        model: PlanModel,
-        data: plan,
-      });
-      return [created, netMap, storageMap];
-    };
-
-    const addOwnerRef = async (model: K8sModel, resource, ownerReferences) => {
-      return await k8sPatch({
-        model,
-        resource,
-        data: [
-          {
-            op: 'add',
-            path: '/metadata/ownerReferences',
-            value: ownerReferences,
-          },
-        ],
-      });
-    };
-
-    Promise.all([createStorage(storageMap), createNetwork(netMap)])
-      .then(([storageMap, netMap]) => {
-        return createPlan(
-          produce(plan, (draft) => {
-            draft.spec.map.network = getObjectRef(netMap);
-            draft.spec.map.storage = getObjectRef(storageMap);
-          }),
-          netMap,
-          storageMap,
-        );
-      })
-      .then(([createdPlan, netMap, storageMap]) => {
-        const ownerReferences = [getObjectRef(createdPlan)];
-        return Promise.all([
-          addOwnerRef(StorageMapModel, storageMap, ownerReferences),
-          addOwnerRef(NetworkMapModel, netMap, ownerReferences),
-        ]);
-      })
-      .then(
-        () =>
-          mounted.current &&
-          history.push(
-            getResourceUrl({
-              reference: PlanModelRef,
-              namespace,
-              name: plan.metadata.name,
-            }),
-          ),
-      )
-      .catch((error) => {
-        mounted.current && dispatch(setError(error));
-      });
-  }, [state.flow.editingDone, state.underConstruction.storageMap]);
+  const [state, dispatch, emptyContext] = useFetchEffects(namespace);
+  useSaveEffect(state, dispatch);
 
   const [isLoading, toggleIsLoading] = useToggle();
   const onUpdate = toggleIsLoading;
